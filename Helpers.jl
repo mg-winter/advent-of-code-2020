ea30b4bed3f9d251a7aebd296ee0528764d65c2f
Ria <Ria@Rias-MacBook.local>
2020-12-03 22:59:18 -0600

    Added helpers, refactored days 2 and 3 accordingly, added blank template

---
+++ Helpers.jl
@@ -0,0 +1,67 @@
+using DelimitedFiles
+using Base
+using Printf
+
+curry(func::Function, first) = args -> func(first, args...)
+
+curry_map(mapfunc::Function) = curry(map, mapfunc)
+
+curry_filter(filterfunc::Function) = curry(filter, filterfunc)
+
+
+readchargrid(file) = permutedims(hcat(map(collect, readlines(file))...))
+
+readgrid(file, delim = nothing, type = nothing) = delim == nothing ? readchargrid(file) : (params = type == nothing ? (file, delim) : (file, delim, type); readdlm(params...))
+    
+readcollection(file, converter::Function = nothing) = (res = readlines(file); converter == nothing ? res : map(converter, res))
+
+
+joinparas(arr) = join(arr, "\n\n")
+
+struct TestPermutation
+    args
+    expected
+end
+
+struct Test
+    func::Function
+    permutation::TestPermutation
+    resultconverter
+
+    Test(func, permutation, resultconverter) = new(func, permutation, resultconverter)
+    Test(func, permutation) = new(func, permutation, nothing)
+end
+
+struct TestResult
+    test::Test
+    actual
+    compare_actual
+    ispass
+end
+
+function run(test::Test)
+    res = test.func(test.permutation.args...)
+    rescompare = test.resultconverter == nothing ? res : test.resultconverter(res)
+    return TestResult(test, res, rescompare, rescompare == test.permutation.expected)
+end
+
+run(tests::AbstractVector{Test}) = map(run, tests)
+
+tests(func, permutations, converterfunc = nothing) = map(p -> Test(func, p, converterfunc), permutations)
+
+run(func, permutations, converterfunc = nothing) = tests(func, permutations, converterfunc) |> run
+
+skipfirst(coll) = (len = size(coll, 1); len > 1 ? coll[2:len] : [])
+
+function tostring(perm::TestPermutation, skipinput::Bool = true)
+    args = skipinput ? skipfirst(perm.args) : perm.args
+
+    return @sprintf("Args: %s\nExpected: %s", args, perm.expected)
+end
+
+tostring(t::Test, skipinput::Bool = true) = @sprintf("Function: %s\n%s\nConverter: %s", t.func, tostring(t.permutation, skipinput), t.resultconverter)
+
+tostring(res::TestResult, skipinput::Bool = true) = @sprintf("%s\nActual: %s\nPass: %s", tostring(res.test, skipinput), res.actual, res.ispass)
+
+tostring(reslist::AbstractVector, skipinput::Bool = true) = map(res -> tostring(res, skipinput), reslist) |> joinparas
+
---
+++ template.jl
@@ -0,0 +1,32 @@
+include("../Helpers.jl")
+
+
+
+function test_a()
+    return []
+end
+
+function test_b()
+    return []
+end
+
+function run_a()
+end
+
+function run_b()
+end
+
+println("Test part A")
+println(tostring(test_a()))
+
+println("")
+println("Result for part A")
+println(run_a())
+
+println("")
+println("Test part B")
+println(tostring(test_a()))
+
+println("")
+println("Result for part B")
+println(run_b())
